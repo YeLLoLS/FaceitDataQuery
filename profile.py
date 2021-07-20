d6543f4e1e500d2cfcd2c9f77e778528334d2f3a
Lungu Silviu <39781744+YeLLoLS@users.noreply.github.com>
2021-07-20 20:23:55 +0300

    rewriting profile

--- profile.py
+++ profile.py
@@ -3,21 +3,31 @@ import os
 import json
 
 key_api = os.environ['api_key']
+
+
 def profile(player_name, game):
-    counter = 0
-    index_valid = 0         
     faceit_data = FaceitData(key_api)
     players = faceit_data.search_players(player_name, game)
+    # print(players)
+    # -------- getting player id --------#
+    counter = 0
+    index_valid = 0
     for el in players["items"]:
-      if el['nickname'] == player_name:
-          index_valid = counter    
-      counter = counter + 1
+        if el['nickname'] == player_name:
+            index_valid = counter
+        counter = counter + 1
     profil = players["items"][int(index_valid)]
     idPlayer = profil['player_id']
-    info_stats = faceit_data.player_id_details(idPlayer)
-    
-    ranking = faceit_data.player_ranking_of_game(game, info_stats['games'][f'{game}']['region'], idPlayer, info_stats['country'])
+    # -------- end --------#
 
+    info_stats = faceit_data.player_id_details(idPlayer)
+    steam_id_64 = info_stats['steam_id_64']
+    afk = info_stats['infractions']['afk']
+    leaver = info_stats['infractions']['leaver']
+
+    # -------- validating the correct player from the list recieved and getting ranking position for player's country --------#
+    ranking = faceit_data.player_ranking_of_game(game, info_stats['games'][f'{game}']['region'], idPlayer,
+                                                 info_stats['country'])
     counter2 = 0
     index_valid2 = 0
     for el in ranking['items']:
@@ -26,6 +36,9 @@ def profile(player_name, game):
         counter2 = counter2 + 1
     correct_player = ranking['items'][index_valid2]
     country_ranking = correct_player['position']
+    # -------- end --------#
+
+    # -------- getting game stats and processing it (replacing 0 with X emoji and 1 with white check mark),(replacing numbers from elo with number emoji's)--------#
     stats = faceit_data.player_stats(player_id=idPlayer, game_id=game)
     winRate = stats['lifetime']['Win Rate %']
     avgKD = stats['lifetime']['Average K/D Ratio']
@@ -49,12 +62,14 @@ def profile(player_name, game):
     elo = str(info_stats['games']['csgo']['faceit_elo'])
     list_elo = []
     for el in elo:
-      list_elo.append(importante['number_emoji'][f'{el}'])
-    
+        list_elo.append(importante['number_emoji'][f'{el}'])
+
     empty_elo = ''
     for el in list_elo:
         empty_elo = empty_elo + el + ' '
-    
+    # -------- end --------#
+
+    # -------- getting general data from player's account --------#
     nickName = profil['nickname']
     status = profil['status']
     nameGame = profil['games'][0]['name']
@@ -66,7 +81,6 @@ def profile(player_name, game):
     else:
         verified = ':x:'
     avatar = profil['avatar']
-    steam_id_64 = info_stats['steam_id_64']
-    afk = info_stats['infractions']['afk']
-    leaver = info_stats['infractions']['leaver']
+    # -------- end --------#
+
     return idPlayer, nickName, nameGame, gameSkill, empty_elo, status, country, verified, avatar, winRate, avgKD, avgHS, lose_wins, longestWINstreak, currentWINstreak, matches, wins, loses, steam_id_64, afk, leaver, country_ranking, elo
